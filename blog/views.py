510400e9240259833429e25604093a72f061e361
ahmad <kafi@Ahmadkafi.localdomain>
2020-09-14 00:25:03 +0300

    add feature to edit post from frontend

--- blog/views.py
+++ blog/views.py
@@ -25,12 +25,24 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-    
-    form = PostForm()
-    stuff_for_frontend = {'form': form}
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
 
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 
